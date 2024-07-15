8ac231f7eec58943f7ac72cdbf58022def0836a6
“neomdavid” <neomanaogdavid@gmail.com>
2024-07-15 23:35:05 +0800

    Solo: display category name per category navigation

--- App.js
+++ App.js
@@ -5,16 +5,44 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverViewScreen from "./screens/MealsOverViewScreen";
 import { NavigationContainer } from "@react-navigation/native";
 
+import { CATEGORIES } from "./data/dummy-data";
+
 const Stack = createNativeStackNavigator();
+function getNameByID(id) {
+  const matchingCategory = CATEGORIES.find((category) => category.id === id);
+  return matchingCategory ? matchingCategory.title : null;
+}
 
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-          <Stack.Screen name="MealsOverview" component={MealsOverViewScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "All Categories",
+            }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverViewScreen}
+            options={({ route, navigation }) => {
+              const catId = route.params.categoryId;
+              const title = getNameByID(catId);
+              return {
+                title: title,
+              };
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
